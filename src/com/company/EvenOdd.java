679868839561f23f8029743f94580e0084e8158a
shirley <shirley.ndou@umuzi.org>
2019-11-13 10:31:11 +0200

    even or odd function

---
+++ src/com/company/EvenOdd.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class EvenOdd {
+
+    public void even_or_odd(int number){
+        if(number % 2== 0){
+            System.out.println(number + " is even");
+        } else{
+            System.out.println(number + " is odd");
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,13 +8,16 @@ public class Main {
 	// write your code here
 
         Scanner scan = new Scanner(System.in);
-        hello obj = new hello();
 
+        hello obj = new hello();
         System.out.println("Enter your name: ");
         String name = scan.nextLine();
-
         obj.hello(name);
 
+        EvenOdd obj2 = new EvenOdd();
+        System.out.println("Enter a number");
+        int number = scan.nextInt();
+        obj2.even_or_odd(number);
     }
 
 
